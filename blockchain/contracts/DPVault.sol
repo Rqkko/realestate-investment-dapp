05950d144a329472aa6c1aafdbd227f2d25ab9b5
Rqkko <rakko26@gmail.com>
2025-04-29 18:12:35 +0700

    fix: DPVault logic
    - include rate (1 ETH = 100 DP)
    - transfer DP ownership to DPVault
    - add additional require statements

--- blockchain/contracts/DPVault.sol
+++ blockchain/contracts/DPVault.sol
@@ -6,6 +6,7 @@ import "./DP.sol";
 
 contract DPVault is Ownable {
     DP public dpToken;
+    uint256 private rate = 100; 
     
     event Deposited(address indexed user, uint256 ethAmount, uint256 dpAmount);
     event Withdrawn(address indexed user, uint256 dpAmount, uint256 ethAmount);
@@ -21,24 +22,22 @@ contract DPVault is Ownable {
 
     function deposit() public payable {
         require(msg.value > 0, "DPVault: ETH amount must be > 0");
-        uint256 dpAmount = msg.value;
+        require(address(msg.sender).balance >= msg.value, "DPVault: Insufficient ETH balance");
+        uint256 dpAmount = msg.value * rate;
         dpToken.mint(msg.sender, dpAmount);
         emit Deposited(msg.sender, msg.value, dpAmount);
     }
 
     function withdraw(uint256 dpAmount) external {
         require(dpAmount > 0, "DPVault: DP amount must be > 0");
-        require(address(this).balance >= dpAmount, "DPVault: Insufficient ETH balance");
+        require(dpToken.balanceOf(msg.sender) >= dpAmount, "DPVault: Insufficient DP balance");
+        require(address(this).balance >= dpAmount/rate, "DPVault: Insufficient ETH in vault");
         dpToken.burnFrom(msg.sender, dpAmount);
-        payable(msg.sender).transfer(dpAmount);
+        payable(msg.sender).transfer(dpAmount/rate);
         emit Withdrawn(msg.sender, dpAmount, dpAmount);
     }
 
     function withdrawETH(uint256 amount) external onlyOwner {
         payable(owner()).transfer(amount);
-    }
-
-    function transferDPOwnership(address newOwner) external onlyOwner {
-    dpToken.transferOwnership(newOwner);
     }
 }
--- blockchain/migrations/3_transfer_ownership.js
+++ blockchain/migrations/3_transfer_ownership.js
@@ -5,7 +5,6 @@ module.exports = async function(deployer, network, accounts) {  // Add accounts 
   const vault = await DPVault.deployed();
   const dp = await DP.deployed();
   
-  // Transfer DP ownership to accounts[0]
-  await vault.transferDPOwnership(accounts[0], {from: accounts[0]});
-  console.log("Transferred DP ownership to:", accounts[0]);
+  // Transfer DP ownership to vault
+  await dp.transferOwnership(vault.address);
 };
