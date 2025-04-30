49a56c76a6bd0d41447e8733351a6a7d7cf10e4d
Rqkko <rakko26@gmail.com>
2025-04-30 22:38:47 +0700

    Refactor DP: remove inital supply

--- blockchain/contracts/DP.sol
+++ blockchain/contracts/DP.sol
@@ -5,11 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DP is ERC20, Ownable {
-    uint256 private constant INITIAL_SUPPLY = 10_000_000 * 10**18; // 10M tokens with 18 decimals
-    
-    constructor() ERC20("DeProp Token", "DP") Ownable(msg.sender) {
-        _mint(msg.sender, INITIAL_SUPPLY);
-    }
+    constructor() ERC20("DeProp Token", "DP") Ownable(msg.sender) {}
 
     /**
      * @dev Mints new tokens to specified address
