2020af480905df6b3a97302cf15ec59918601ea7
Rqkko <rakko26@gmail.com>
2025-04-30 23:26:47 +0700

    Refactor investment-summary.tsx: change color & font

--- client/src/components/investment-summary.tsx
+++ client/src/components/investment-summary.tsx
@@ -5,37 +5,37 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 export function InvestmentSummary() {
   return (
     <div className="gap-4 grid md:grid-cols-2 lg:grid-cols-3">
-      <Card>
+      <Card className="bg-[#020C3B]">
         <CardHeader className="flex flex-row justify-between items-center space-y-0 pb-2">
-          <CardTitle className="font-medium text-sm">Total DP Invested</CardTitle>
+          <CardTitle className="font-bold text-sm text-white">Total DP Invested</CardTitle>
           <Coins className="w-4 h-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="font-bold text-2xl">2,500 DP</div>
+          <div className="font-bold text-2xl text-white">2,500 DP</div>
           {/* <p className="text-muted-foreground text-xs">
             <span className="text-green-500">+500 DP</span> from last month
           </p> */}
         </CardContent>
       </Card>
-      <Card>
+      <Card className="bg-[#020C3B]">
         <CardHeader className="flex flex-row justify-between items-center space-y-0 pb-2">
-          <CardTitle className="font-medium text-sm">Projects Invested</CardTitle>
+          <CardTitle className="font-bold text-sm text-white">Projects Invested</CardTitle>
           <Building className="w-4 h-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="font-bold text-2xl">3</div>
+          <div className="font-bold text-2xl text-white">3</div>
           {/* <p className="text-muted-foreground text-xs">
             <span className="text-green-500">+1</span> from last month
           </p> */}
         </CardContent>
       </Card>
-      <Card>
+      <Card className="bg-[#020C3B]">
         <CardHeader className="flex flex-row justify-between items-center space-y-0 pb-2">
-          <CardTitle className="font-medium text-sm">Total Earnings</CardTitle>
+          <CardTitle className="font-bold text-sm text-white">Total Earnings</CardTitle>
           <TrendingUp className="w-4 h-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
-          <div className="font-bold text-2xl">1,030 DP</div>
+          <div className="font-bold text-2xl text-white">1,030 DP</div>
           {/* <p className="text-muted-foreground text-xs">
             <span className="text-green-500">+210 DP</span> from last month
           </p> */}
