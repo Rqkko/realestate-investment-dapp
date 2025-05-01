ceb400a37c8f81e5017cd781570cc12a7ee5a3c7
Wongsathorn Chengcharoen <52569446+Rqkko@users.noreply.github.com>
2025-05-01 17:45:06 +0700

    Style 2_deploy_contracts.js: remove whitespace

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const Project = artifacts.require("Project");
 const DP = artifacts.require("DP");
 const DPVault = artifacts.require("DPVault");
 
-module.exports = async function (deployer) {
+module.exports = async function(deployer) {
   deployer.deploy(DP).then(async () => {
     const dp = await DP.deployed();
     await deployer.deploy(DPVault, dp.address);
