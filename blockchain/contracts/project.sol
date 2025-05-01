c4826631d105389e3cafdbcba8c506ffde600ba5
Surapat Petchang <npetchang@gmail.com>
2025-05-01 11:22:00 +0700

    Update project.sol

--- blockchain/contracts/project.sol
+++ blockchain/contracts/project.sol
@@ -2,31 +2,38 @@
 pragma solidity ^0.8.0;
 
 contract Project {
-    // Track each user's stake
-    mapping(address => uint256) public stakes;
+    mapping(address => uint256) public stakes;               // Track how much each user has invested
+    address[] public investors;                              // Store all investor addresses
 
-    // Invest function
-    function invest() public payable {
-        stakes[msg.sender] += msg.value;
+    function invest() public payable {                       // Users invest Ether into the contract
+        if (stakes[msg.sender] == 0) {                       // First-time investor? Add to list
+            investors.push(msg.sender);
+        }
+        stakes[msg.sender] += msg.value;                     // Increase their stake
     }
 
-    // Get your stake
     function getStake(address user) public view returns (uint256) {
-        return stakes[user];
+        return stakes[user];                                 // Return user's invested amount
     }
 
-    // Transfer stake to another
     function transferStake(address to, uint256 amount) public {
-        require(stakes[msg.sender] >= amount, "Not enough stake");
-        stakes[msg.sender] -= amount;
-        stakes[to] += amount;
+        require(stakes[msg.sender] >= amount, "Not enough stake"); // Ensure sender has enough stake
+
+        if (stakes[to] == 0) {                               // New recipient? Add to investors
+            investors.push(to);
+        }
+
+        stakes[msg.sender] -= amount;                        // Deduct from sender
+        stakes[to] += amount;                                // Add to recipient
     }
 
-    // Distribute dividends to all (example: 1% of each stake)
-    //function distributeDividend(amonunt...????) public {
-        // This will need **more logic** (you can add later)
-        // for loop : for inverstors...???
-    //}
-}
+    function distributeDividend() public {
+        for (uint i = 0; i < investors.length; i++) {        // Loop through all investors
+            address investor = investors[i];                 // Get investor address
+            uint256 dividend = stakes[investor] / 100;       // Calculate 1% dividend
+            payable(investor).transfer(dividend);            // Send dividend
+        }
+    }
 
-// needed money (100 DP) : raised money (35 DP/100 DP) -> 35% 
+    receive() external payable {}                            // Allow contract to receive Ether
+}
