ff1012e02a0ec5ae8ba13f24e81eb27a2a43e8ea
Rqkko <rakko26@gmail.com>
2025-05-02 09:54:07 +0700

    Create ProjectFactory.sol: add core features

---
+++ blockchain/contracts/ProjectFactory.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Project.sol";
+
+contract ProjectFactory {
+    address[] public projects;
+
+    event ProjectCreated(address projectAddress, address creator);
+
+    function createProject(uint256 amountNeeded) external {
+        Project project = new Project();
+        project.setAmountNeeded(amountNeeded);
+        projects.push(address(project));
+        emit ProjectCreated(address(project), msg.sender);
+    }
+
+    function getAllProjects() external view returns (address[] memory) {
+        return projects;
+    }
+
+    function getProject(uint index) external view returns (address) {
+        require(index < projects.length, "Invalid index");
+        return projects[index];
+    }
+
+    function getProjectCount() external view returns (uint256) {
+        return projects.length;
+    }
+}
