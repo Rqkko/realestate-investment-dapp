e143a9ece14fccf88066876b98b20d21225ee53a
Rqkko <rakko26@gmail.com>
2025-05-02 09:59:54 +0700

    Create 4_seed_data.js

---
+++ blockchain/migrations/4_seed_data.js
@@ -0,0 +1,10 @@
+
+module.exports = async function(deployer) {
+  // TODO: Make initial test data
+
+  // let factory = await ProjectFactory.deployed()
+  // await factory.createProject(web3.utils.toWei("5", "ether"), { from: accounts[0] })
+  // let addresses = await factory.getAllProjects()
+  // let project = await Project.at(addresses[0])
+  // await project.invest({ from: accounts[1], value: web3.utils.toWei("1", "ether") })
+}
