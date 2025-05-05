2d6aa7d0c486c8b6c205a5fd018ca7a575d97386
Rqkko <rakko26@gmail.com>
2025-05-05 12:28:51 +0700

    feat: integrate DP into Project

--- blockchain/contracts/Project.sol
+++ blockchain/contracts/Project.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./DP.sol";
+import "./DPVault.sol";
+
 contract Project {
-    mapping(address => uint256) public stakes;
+    mapping(address => uint256) public stakes;  // out of 10000 (To allow decimal in frontend)
     string public name;
     string public description;
     // string public image;
@@ -13,32 +16,55 @@ contract Project {
     enum ProjectStatus { RaisingFunds, Building, Completed }
     ProjectStatus public status;
 
-    constructor(string memory _name, string memory _description, string memory _location) {
+    DP public dpToken;
+    DPVault public dpVault;
+
+    constructor(
+        string memory _name,
+        string memory _description,
+        string memory _location,
+        address _dpTokenAddress,
+        address _dpVaultAddress
+    ) {
         name = _name;
         description = _description;
         location = _location;
         status = ProjectStatus.RaisingFunds;
+        dpToken = DP(_dpTokenAddress);
+        dpVault = DPVault(payable(_dpVaultAddress));
     }
 
     function setAmountNeeded(uint256 _amountNeeded) public {
         amountNeeded = _amountNeeded;
     }
 
-    // TODO make 'setStatus' function
+    function setStatus(ProjectStatus _status) public {
+        status = _status;
+    }
 
     // TODO Investor can sell their stake for DP
     //      For now, investor can sell their stake without another investor (sell to "Project")
 
-    // TODO: Change every part that utilizes ETH to DP
-    function invest() public payable {
-        require(msg.value <= amountNeeded, "Investment exceeds amount needed");
-        require(msg.value > 0, "Investment must be greater than 0");
-        // Add investor to list (if not already present)
+    function invest(uint256 dpAmount) public {
+        require(status == ProjectStatus.RaisingFunds, "Project is not raising funds");
+        require(dpAmount > 0, "Investment must be greater than 0");
+        require(amountRaised + dpAmount <= amountNeeded, "Investment exceeds amount needed");
+
+        // Transfer DP tokens from the investor to the project
+        dpToken.transferFrom(msg.sender, address(this), dpAmount);
+
+        // Add investor to the list (if not already present)
         if (stakes[msg.sender] == 0) {
             investors.push(msg.sender);
         }
-        stakes[msg.sender] += msg.value*100/amountNeeded;
-        amountRaised += msg.value;
+
+        stakes[msg.sender] += dpAmount * 10000 / amountNeeded;
+        amountRaised += dpAmount;
+
+        // Automatically change status to Building if funding goal is met
+        if (amountRaised >= amountNeeded) {
+            status = ProjectStatus.Building;
+        }
     }
 
     function getStake(address user) public view returns (uint256) {
@@ -59,8 +85,8 @@ contract Project {
     function distributeDividend(uint256 amount) public {
         for (uint256 i = 0; i < investors.length; i++) {
             address investor = investors[i];
-            uint256 dividend = amount * stakes[investor] / 100;
-            payable(investor).transfer(dividend);
+            uint256 dividend = amount * stakes[investor] / 10000;
+            dpToken.transfer(investor, dividend);
         }
     }
     
