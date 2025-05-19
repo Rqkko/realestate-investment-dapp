164a5ec7289dfb517c2d250bb98cc149c0972a61
Rqkko <rakko26@gmail.com>
2025-05-19 13:43:27 +0700

    feat: create home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,32 +1,48 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Navbar from "./components/navbar";
+import realestate1 from "../../public/realestates/realestate_1.jpg";
+import realestate2 from "../../public/realestates/realestate_2.jpg";
+import realestate3 from "../../public/realestates/realestate_3.jpg";
+import realestate4 from "../../public/realestates/realestate_4.jpg";
+import Image from "next/image";
 
-export default function GoButton() {
+const images = [realestate1, realestate2, realestate3, realestate4];
+
+export default function HomePage() {
   const router = useRouter();
 
-  // const handleClick = () => {
-  //   router.push("/invest");
-  // };
+  return (
+    <div className="bg-gradient-to-b from-[#0f1c2e] to-black min-h-screen font-sans text-white">
+      <Navbar />
 
-  useEffect(() => {
-    router.push("/projects");
-  })
+      <div className="flex flex-col justify-center items-center mx-auto px-20 pt-20 max-w-6xl min-h-screen">
+        <h1 className="mb-4 font-bold text-4xl">Welcome to DeProp!</h1>
 
-  // return (
-  //   <div className="flex flex-col justify-center items-center bg-gray-200 p-6 min-h-screen text-center">
-  //     <h1 className="mb-4 font-semibold text-4xl text-gray-800">
-  //       Welcome to DeProp!
-  //     </h1>
-  //     <p className="mb-6 text-gray-600 text-xl">
-  //       Start investing in exciting real estate projects today.
-  //     </p>
-  //     <button
-  //       onClick={handleClick}
-  //       className="bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-md text-lg text-white transition-colors"
-  //     >
-  //       Go to Invest Page
-  //     </button>
-  //   </div>
-  // );
+        <p className="mb-8 text-gray-300 text-xl">
+          Start investing in exciting real estate projects today.
+        </p>
+
+        <div className="flex gap-4 mb-8">
+          {images.map((img, idx) => (
+            <Image
+              key={idx}
+              src={img}
+              alt={`Real estate ${idx + 1}`}
+              width={200}
+              height={80}
+              className="rounded-lg object-cover"
+            />
+          ))}
+        </div>
+
+        <button
+          onClick={() => router.push("/projects")}
+          className="bg-white hover:bg-gray-200 px-4 py-2 rounded font-bold text-black"
+        >
+          Explore Projects
+        </button>
+      </div>
+    </div>
+  );
 }
