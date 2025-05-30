e6614d12a4bfeb4d8efbae59ce32b8d5fb977371
Rqkko <rakko26@gmail.com>
2025-05-30 13:40:29 +0700

    chore: remove example files

--- blockchain/migrations/2_deploy_contracts.js
+++ blockchain/migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-const Project = artifacts.require("Project");
 const ProjectFactory = artifacts.require("ProjectFactory");
 const DP = artifacts.require("DP");
 const DPVault = artifacts.require("DPVault");
@@ -8,6 +7,5 @@ module.exports = async function(deployer) {
   const dp = await DP.deployed();
   await deployer.deploy(DPVault, dp.address);
   const vault = await DPVault.deployed();
-  // await deployer.deploy(Project, "Project A", "Description", "Location", dp.address, vault.address);
   await deployer.deploy(ProjectFactory, dp.address, vault.address);
 };
--- client/src/lib/contract.ts
+++ client/src/lib/contract.ts
@@ -2,7 +2,6 @@ import { ethers } from "ethers";
 import { MetaMaskInpageProvider } from "@metamask/providers";
 import type { ExternalProvider } from "@ethersproject/providers";
 
-import ExampleABI from "./contracts/Example.json";
 import DPABI from "./contracts/DP.json";
 import DPVaultABI from "./contracts/DPVault.json";
 import ProjectFactoryABI from "./contracts/ProjectFactory.json";
@@ -21,7 +20,6 @@ declare global {
 
 let provider: ethers.providers.Web3Provider | undefined;
 let account: string | undefined;
-let exampleContract: ethers.Contract | undefined;
 let dpContract: ethers.Contract | undefined;
 let dpVaultContract: ethers.Contract | undefined;
 let projectFactoryContract: ethers.Contract | undefined;
@@ -36,26 +34,22 @@ if (typeof window !== "undefined") {
   account = (await provider.send("eth_requestAccounts", []))[0];
   
   // Get the contract address from the ABI
-  let exampleContractAddress: string;
   let dpContractAddress: string;
   let dpVaultContractAddress: string;
   let projectFactoryContractAddress: string;
   try {
-    exampleContractAddress = (ExampleABI.networks as NetworkMap)[networkId].address;
     dpContractAddress = (DPABI.networks as NetworkMap)[networkId].address;
     dpVaultContractAddress = (DPVaultABI.networks as NetworkMap)[networkId].address;
     projectFactoryContractAddress = (ProjectFactoryABI.networks as NetworkMap)[networkId].address;
   } catch { 
     console.log("Invalid networkId, using default contract address (1337)");
-    exampleContractAddress = (ExampleABI.networks as NetworkMap)["1337"].address;
     dpContractAddress = (DPABI.networks as NetworkMap)["1337"].address;
     dpVaultContractAddress = (DPVaultABI.networks as NetworkMap)["1337"].address;
     projectFactoryContractAddress = (ProjectFactoryABI.networks as NetworkMap)["1337"].address;
   }
-  exampleContract = new ethers.Contract(exampleContractAddress, ExampleABI.abi, signer);
   dpContract = new ethers.Contract(dpContractAddress, DPABI.abi, signer);
   dpVaultContract = new ethers.Contract(dpVaultContractAddress, DPVaultABI.abi, signer);
   projectFactoryContract = new ethers.Contract(projectFactoryContractAddress, ProjectFactoryABI.abi, signer);
 }
 
-export { provider, account, exampleContract, dpContract, dpVaultContract, projectFactoryContract };
+export { provider, account, dpContract, dpVaultContract, projectFactoryContract };
