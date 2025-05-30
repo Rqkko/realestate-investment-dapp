3a07cbf0baa4e055fee13c809a8dc814a2abf649
Rqkko <rakko26@gmail.com>
2025-05-31 00:00:21 +0700

    fix: metadata name taken in contract

--- blockchain/contracts/Project.sol
+++ blockchain/contracts/Project.sol
@@ -3,14 +3,12 @@ pragma solidity ^0.8.0;
 
 import "./DP.sol";
 import "./DPVault.sol";
+import "./ProjectMetadata.sol";
 
 contract Project {
     mapping(address => uint256) public stakes;  // out of 10000 (To allow decimal in frontend)
     mapping(address => uint256) public earnings;
-    string public name;
-    string public description;
-    // string public image;
-    string public location;
+    ProjectMetadata public projectMetadata;
     address[] public investors;
     uint256 public amountNeeded;
     uint256 public amountRaised;
@@ -23,11 +21,11 @@ contract Project {
     event ContributionReceived(address indexed contributor, uint256 dpAmount);
 
     constructor(
-        address _metadata,
+        address _projectMetadata,
         address _dpTokenAddress,
         address _dpVaultAddress
     ) {
-        metadata = ProjectMetadata(_metadata);
+        projectMetadata = ProjectMetadata(_projectMetadata);
         status = ProjectStatus.RaisingFunds;
         dpToken = DP(_dpTokenAddress);
         dpVault = DPVault(payable(_dpVaultAddress));
