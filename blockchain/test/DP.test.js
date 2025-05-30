64983fe565be1dcbd2aaa01b01cae0227dab0637
Rqkko <rakko26@gmail.com>
2025-05-30 17:15:08 +0700

    test: add DP test

--- blockchain/test/DP.test.js
+++ blockchain/test/DP.test.js
@@ -1,26 +1,48 @@
-// TODO Fix this (If have time)
 const DP = artifacts.require("DP");
+const DPVault = artifacts.require("DPVault");
 
-contract("DP", (accounts) => {
-  let dp;
+contract("DPVault", (accounts) => {
+  let dp, vault;
   const owner = accounts[0];
-  const initialSupply = "10000000000000000000000000"; // 10M tokens in wei (string format)
+  const user = accounts[1];
 
   before(async () => {
-    dp = await DP.new();
+    dp = await DP.deployed();
+    vault = await DPVault.deployed();
   });
 
-  it("should mint initial supply to owner", async () => {
-    const balance = await dp.balanceOf(owner);
-    assert.equal(balance.toString(), initialSupply);
+  it("should allow deposit of ETH and mint DP tokens", async () => {
+    const depositAmount = web3.utils.toWei("1", "ether");
+    await vault.deposit({ from: user, value: depositAmount });
+
+    const dpBalance = await dp.balanceOf(user);
+    // Assuming 1 ETH = 100 DP tokens (with 18 decimals)
+    assert.equal(dpBalance, 100, "User should receive 100 DP tokens");
   });
 
-  it("should allow token burning", async () => {
-    const burnAmount = "1000000000000000000"; // 1 token in wei
-    const expectedBalance = "9999999000000000000000000"; // 10M - 1 in wei
-    
-    await dp.burn(burnAmount, { from: owner });
-    const balance = await dp.balanceOf(owner);
-    assert.equal(balance.toString(), expectedBalance);
+  it("should allow withdrawal of DP tokens for ETH", async () => {
+    // Approve vault to spend user's DP tokens
+    await dp.approve(vault.address, 100, { from: user });
+
+    // Withdraw 100 DP tokens (should receive 1 ETH)
+    const initialEthBalance = web3.utils.toBN(await web3.eth.getBalance(user));
+    const tx = await vault.withdraw(100, { from: user });
+    const gasUsed = web3.utils.toBN(tx.receipt.gasUsed);
+    const txDetails = await web3.eth.getTransaction(tx.tx);
+    const gasPrice = web3.utils.toBN(txDetails.gasPrice);
+    const gasCost = gasUsed.mul(gasPrice);
+
+    const finalEthBalance = web3.utils.toBN(await web3.eth.getBalance(user));
+    // User should receive 1 ETH minus gas cost
+    assert(
+      finalEthBalance.sub(initialEthBalance).add(gasCost).eq(web3.utils.toBN(web3.utils.toWei("1", "ether"))),
+      "User should receive 1 ETH for 100 DP tokens"
+    );
+  });
+
+  it("should update DPVault ETH balance correctly", async () => {
+    const vaultBalance = await web3.eth.getBalance(vault.address);
+    // After deposit and withdrawal, vault should have 0 ETH
+    assert.equal(web3.utils.fromWei(vaultBalance, "ether"), "0", "Vault ETH balance should be 0");
   });
 });
